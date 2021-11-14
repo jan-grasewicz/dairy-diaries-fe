17d2c96433a111a7af0f8248851008334641751f
jan-grasewicz <45798702+jan-grasewicz@users.noreply.github.com>
2021-11-14 18:22:48 +0100

    google sheets basic data fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,35 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
+import { google } from 'googleapis'
 
-const IndexPage = () => (
-  <Layout>
-    <main>
-      <p>main page</p>
-    </main>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <main>
+        <p>main page</p>
+        <pre>{JSON.stringify(data)}</pre>
+      </main>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps() {
+  const auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+  })
+
+  const sheets = google.sheets({ version: 'v4', auth })
+
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range: 'Sheet1!A2:A10',
+    majorDimension: 'COLUMNS',
+  })
+
+  return {
+    props: {
+      data: res.data.values,
+    },
+  }
+}
 
 export default IndexPage
